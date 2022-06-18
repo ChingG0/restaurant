42b4e1a834f3db53dc192fc725a060ac754bf640
ChingG0 <www.karta2428620@gmail.com>
2022-06-18 16:46:52 +0800

    navbar

--- public/javascripts/rating.js
+++ public/javascripts/rating.js
@@ -1,27 +1,36 @@
-ratings = 
-    {
-    Sababa : 4.1,
-    Umeko: 4.3,
-    Ziga: 4.2,
-    Apoint: 4.2,
-    Gusto: 4.7,
-    WXYZ: 4.3,
-    Fika: 4.3,
-    Bravo: 4.7,
-    }
+function getRatings() {
+  const data_id = document.querySelectorAll(".col-sm-4");
 
-const starTotal = 5
+  const starTotal = 5;
 
-document.addEventListener('DOMContentLoaded',getRatings)
+  const resdata = [];
 
-function getRatings(){
-    for(let rating in ratings){
+  let starper = [];
 
-        const starPercentage = (ratings[rating]/ starTotal) * 100
+  axios
+    .get("https://api.jsonbin.io/b/62ad4f04402a5b38022cf772")
+    .then((res) => {
+      resdata.push(...res.data.results);
 
-        const starPercentageRounded = `${Math.round(starPercentage / 10)*10}%`
+      for (let f = 0; f < resdata.length; f++) {
+        star = [];
 
-        //Set width of stars-inner to percentage
-        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded
-    }
-} 
+        const starPercentage = (`${resdata[f].rating}` / starTotal) * 100;
+        // Set width of stars-inner to percentage
+        const starPercentageRounded = `${
+          Math.round(starPercentage / 10) * 10
+        }%`;
+        star += starPercentageRounded;
+        starper.push(star);
+      }
+      for (let i = 0; i < data_id.length; i++) {
+        document.querySelector(`.${data_id[i].dataset.target} .stars-inner`).style.width =
+          starper[i];
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", getRatings);
